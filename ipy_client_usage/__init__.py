94951de67bb5a0cbb58140bd08d8826a985fdb98
Michael McNeil Forbes <mforbes@physics.ubc.ca>
2015-06-16 06:53:04 -0700

    ENH: Make dump write to another file first, then move the file. - If the dump takes a long time, then the file might almost   never be visible in its complete form. - Changed sample_frequency to always be more than 2*longer   than it takes to dump the file.

--- ipy_client_usage/__init__.py
+++ ipy_client_usage/__init__.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import json
+import shutil
 import time
 
 
@@ -10,14 +11,24 @@ def json_datetime(obj):
         raise TypeError("Type {} not serializable".format(type(obj)))
 
 
-def dump(fh, client, uuid_to_name=None):
-    uuid_to_name = uuid_to_name or {}
+def dump(fname, client, uuid_to_name=None):
+    """Dump the data to the file fname and return the time this takes."""
+    # Write to a temporary file, then move it.  This makes the
+    # complete file available more often in the case that there is a
+    # lot of data and dumping takes a while.
+    tic = time.time()
+    temp_name = fname + '_tmp'
+    with open(temp_name, 'w') as fh:
+        uuid_to_name = uuid_to_name or {}
 
-    ids = client.history
-    result = client.get_result(ids)
-    names = [uuid_to_name.get(job_id, job_id) for job_id in ids]
-    metadata = dict(zip(ids, zip(names, result.metadata)))
-    json.dump(metadata, fh, default=json_datetime)
+        ids = client.history
+        result = client.get_result(ids)
+        names = [uuid_to_name.get(job_id, job_id) for job_id in ids]
+        metadata = dict(zip(ids, zip(names, result.metadata)))
+        json.dump(metadata, fh, default=json_datetime)
+
+    shutil.move(temp_name, fname)
+    return time.time() - tic
 
 
 def wait_and_dump(fname, client, sample_frequency=0.5, uuid_to_name=None,
@@ -25,14 +36,13 @@ def wait_and_dump(fname, client, sample_frequency=0.5, uuid_to_name=None,
     # A close lift from Client.wait.
     tic = time.time()
     theids = client.outstanding
-    client.spin()
-    with open(fname, 'w') as fh:
-        dump(fh, client, uuid_to_name=uuid_to_name)
+    dump_time = dump(fname, client, uuid_to_name=uuid_to_name)
+    last_time = time.time()
     while theids.intersection(client.outstanding):
         if timeout >= 0 and (time.time() - tic) > timeout:
             break
-        time.sleep(sample_frequency)
+        time_since_last_dump = time.time() - last_time
+        time.sleep(max(sample_frequency, 2*dump_time) - time_since_last_dump)
         client.spin()
-        with open(fname, 'w') as fh:
-            dump(fh, client, uuid_to_name=uuid_to_name)
+        dump_time = dump(fname, client, uuid_to_name=uuid_to_name)
     return len(theids.intersection(client.outstanding)) == 0
